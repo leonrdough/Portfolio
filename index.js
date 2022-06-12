472d50b68d34e3921d23858e6b3ed0f98b1d2f87
leonrdough <leonrdough@gmail.com>
2022-06-12 15:17:46 -0700

    RESPONSIVE UPDATE

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 // 0W9tGCbfyGYwEmuic
 let contrastToggle = false
 let isModalOpen = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape")
+    const x = event.clientX * scaleFactor
+    const y = event.clientY * scaleFactor
+    console.log(x, y)
+
+    for (i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0
+        const boolInt = isOdd ? -1 : 1
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
 
 function toggleContrast() {
     contrastToggle = !contrastToggle
